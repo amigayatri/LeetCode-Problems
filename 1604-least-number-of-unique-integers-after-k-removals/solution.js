8ffcc9a943b2a375c3b4df0f5ee241e2660d73ad
Ami Gayatri <miragayatri@gmail.com>
2024-06-19 04:20:41 +0000

    [LeetCode Sync] Runtime - 97 ms (93.10%), Memory - 74 MB (68.97%)

---
+++ 1604-least-number-of-unique-integers-after-k-removals/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+    const n = arr.length
+    let count = new Map()
+    let maxFrequency = 1
+    for (let i = 0; i < n; i++) {
+        const c = arr[i]
+        if (!count.has(c)) {
+            count.set(c, 1)
+        } else {
+            count.set(c, count.get(c) + 1)
+            maxFrequency = Math.max(maxFrequency, count.get(c))
+        }
+    }
+    let countOfFrequencies = new Array(maxFrequency+1).fill(0)
+    for (let [c, quantity] of count.entries()) {
+        countOfFrequencies[quantity]++
+    }
+    let remainingUniqueElements = count.size
+    for (let frequency = 1; frequency < maxFrequency+1; frequency++) {
+        const count = countOfFrequencies[frequency]
+        const numElementsToRemove = Math.min(Math.floor(k/frequency), count)
+        k -= frequency*numElementsToRemove
+        remainingUniqueElements -= numElementsToRemove
+        if (k < frequency) return remainingUniqueElements
+    }
+    return 0
+};
