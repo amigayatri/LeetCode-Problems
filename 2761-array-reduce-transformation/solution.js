1c49542f2f4b624595c4d437b3ec51657c3d7d84
Ami Gayatri <miragayatri@gmail.com>
2024-07-08 08:21:42 +0000

    [LeetCode Sync] Runtime - 60 ms (21.24%), Memory - 49.3 MB (49.90%)

---
+++ 2761-array-reduce-transformation/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    const n = nums.length
+    if (n == 0) return init
+    let val = fn(init, nums[0])
+    for (let i = 1; i < n; i++) {
+        val = fn(val, nums[i])
+    }
+    return val
+};
