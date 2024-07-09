cc9bc43327107784f6e1bec14b1d7f5323da4838
Ami Gayatri <miragayatri@gmail.com>
2024-07-09 01:42:28 +0000

    [LeetCode Sync] Runtime - 73 ms (6.55%), Memory - 49.9 MB (10.91%)

---
+++ 2749-promise-time-limit/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise(async (resolve, reject) => {
+            const timeout = setTimeout(() => reject("Time Limit Exceeded"), t)
+            try {
+                const result = await fn(...args)
+                resolve(result)
+            } catch (err) {
+                reject(err)
+            }
+            clearTimeout(timeout)
+        })
+    }
+}
