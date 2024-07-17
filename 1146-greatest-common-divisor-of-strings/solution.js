d376a160aea164e16748ce7360496de7831bc299
Ami Gayatri <miragayatri@gmail.com>
2024-07-17 18:00:02 +0000

    [LeetCode Sync] Runtime - 54 ms (54.47%), Memory - 49.5 MB (37.70%)

---
+++ 1146-greatest-common-divisor-of-strings/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+const gcd = (x, y) => {
+    if (y ==0) return x
+    else return gcd(y, x%y)
+}
+var gcdOfStrings = function(str1, str2) {
+    if (str1+str2 != str2+str1) {
+        return ""
+    }
+
+    let gcdLength = gcd(str1.length, str2.length)
+    return str1.substring(0, gcdLength)
+};
