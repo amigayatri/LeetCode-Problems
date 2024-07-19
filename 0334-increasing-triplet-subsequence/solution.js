c01f7779a5f2eaba220679ccdb8afcedf6c28a83
Ami Gayatri <miragayatri@gmail.com>
2024-07-19 03:13:44 +0000

    [LeetCode Sync] Runtime - 83 ms (27.84%), Memory - 69.4 MB (22.59%)

---
+++ 0334-increasing-triplet-subsequence/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let first = Number.MAX_SAFE_INTEGER, second = Number.MAX_SAFE_INTEGER
+    for (let num of nums) {
+        if (num <= first) {
+            first = num
+        } else if (num <= second) {
+            second = num
+        } else {
+            return true
+        }
+    }
+    return false
+};
