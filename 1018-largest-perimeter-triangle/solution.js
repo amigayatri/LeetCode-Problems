bc0d93c0cb4f523f9afe040457604f3def27f2e6
Ami Gayatri <miragayatri@gmail.com>
2024-08-14 22:50:49 +0000

    [LeetCode Sync] Runtime - 81 ms (66.02%), Memory - 52.7 MB (50.00%)

---
+++ 1018-largest-perimeter-triangle/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a, b)=>b-a)
+    const n = nums.length
+    for (let i = 0; i < n-2; i++) {
+        if (nums[i] < nums[i+1]+nums[i+2]) {
+            return nums[i]+nums[i+1]+nums[i+2]
+        }
+    }
+    return 0
+};
