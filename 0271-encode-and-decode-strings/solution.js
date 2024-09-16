5bdf8ca867fe5a99e74ce1ebd037fb16e5f0d11d
Ami Gayatri <miragayatri@gmail.com>
2024-09-16 16:55:30 +0000

    [LeetCode Sync] Runtime - 67 ms (73.70%), Memory - 55.6 MB (65.58%)

---
+++ 0271-encode-and-decode-strings/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Encodes a list of strings to a single string.
+ *
+ * @param {string[]} strs
+ * @return {string}
+ */
+var encode = function(strs) {
+    return strs.join('\0')
+};
+
+/**
+ * Decodes a single string to a list of strings.
+ *
+ * @param {string} s
+ * @return {string[]}
+ */
+var decode = function(s) {
+    return s.split('\0')
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(strs));
+ */
